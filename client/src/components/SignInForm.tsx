ad7c4cc11d014e8ed5f21890df31ea646e53c16e
SagWel <nicolasbensault@free.fr>
2026-02-11 09:48:14 +0100

    *

--- client/src/components/SignInForm.tsx
+++ client/src/components/SignInForm.tsx
@@ -1,75 +1,12 @@
-<<<<<<< HEAD
-import { Flex, Heading, Input, Link, Stack, Text } from "@chakra-ui/react"
-import { useNavigate } from "react-router-dom"
-=======
 import { useState } from "react";
 import { Flex, Heading, Input, Link, Stack, Text } from "@chakra-ui/react";
->>>>>>> 3d360e747966873fde27966532640a523daa66c5
 
 const SignInForm = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-<<<<<<< HEAD
-const SignInForm: React.FC<ISignInFormProps> = () => {
-    const navigate = useNavigate()
-
-    const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault()
-
-        navigate("/home")
-    }
-
-    return (
-        <Flex flexDir={"column"} justifyContent={"start"} alignItems={"center"}
-        h={"610px"} w={"900px"}
-        textAlign={"center"}
-        bg={"#909E9660"}
-        borderRadius={"20px"}>
-            <Flex flexDir={"column"} justifyContent={"center"} alignItems={"center"} gap={"20px"}
-            mt={"30px"}>
-                <Heading fontSize={"64px"} fontWeight={"700"} mb={"5px"}>sign in</Heading>
-                <Text color={"black"} fontSize={"24px"}>
-                    Don't have an account ?
-                    <Link href="/register"
-                    ml={"5px"}
-                    color={"black"}>
-                        Sign up
-                    </Link>
-                </Text>
-                <form onSubmit={handleSubmit}>
-                    <Stack gap={12}>
-                            <Input type="email"
-                            h={"100px"} w={"690px"}
-                            color={"black"} fontSize={"32px"}
-                            bg={"#D9D9D9"}
-                            borderColor={"#D9D9D9"} borderRadius={"20px"}
-                            placeholder="email"
-                            _placeholder={{
-                                textAlign: "center",
-                                color: "black"
-                            }}/>
-                            <Input type="password"
-                            h={"100px"} w={"690px"}
-                            color={"black"} fontSize={"32px"}
-                            bg={"#D9D9D9"}
-                            borderColor={"#D9D9D9"} borderRadius={"20px"}
-                            placeholder="mot de passe"
-                            _placeholder={{
-                                textAlign: "center",
-                                color: "black"
-                            }}/>
-                    </Stack>
-                    <button type="submit" style={{display: "none"}}></button>
-                </form>
-            </Flex>
-        </Flex>
-    )
-}
-=======
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
->>>>>>> 3d360e747966873fde27966532640a523daa66c5
 
     console.log("EMAIL =", email);
     console.log("PASSWORD =", password);
@@ -92,7 +29,7 @@ const SignInForm: React.FC<ISignInFormProps> = () => {
 
       if (res.ok) {
         localStorage.setItem("token", data.token);
-        window.location.href = "/home";
+        globalThis.location.href = "/home";
       } else {
         alert(data.message);
       }
